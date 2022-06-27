8a9adeebce9a218a41917e648246e7b7586a1ec3
Gui_T <guilherme.gui.tama2gmail.com>
2022-06-27 15:15:10 -0300

    Soozza

---
+++ src/components/templates/LivrosComp.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="livros">
+    <div class="livro">
+      <img
+        src="https://images2.alphacoders.com/261/thumb-1920-26102.jpg"
+        alt=""
+      />
+    </div>
+    <div class="livro">
+      <img src="" alt="" />
+    </div>
+    <div class="livro">
+      <img src="" alt="" />
+    </div>
+    <div class="livro">
+      <img src="" alt="" />
+    </div>
+    <div class="livro">
+      <img src="" alt="" />
+    </div>
+    <div class="livro">
+      <img src="" alt="" />
+    </div>
+    <div class="livro">
+      <img src="" alt="" />
+    </div>
+    <div class="livro">
+      <img src="" alt="" />
+    </div>
+  </main>
+</template>
+<style>
+.livro img {
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script>
+import LivrosComp from "@/components/templates/LivrosComp.vue";
+</script>
+<template>
+  <main class="home1">Bem-Vindo</main>
+  <LivrosComp> </LivrosComp>
+</template>
+<style>
+.home1 {
+  font-size: 100px;
+  text-align: center;
+  color: white;
+}
+</style>
